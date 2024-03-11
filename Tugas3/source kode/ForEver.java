75e352c6299b2d7dc4128a6c87b66189eceb4182
Shabran29 <160574329+Shabran29@users.noreply.github.com>
2024-03-11 21:20:50 +0800

    Add files via upload

---
+++ Tugas3/source kode/ForEver.java
@@ -0,0 +1,18 @@
+//Nama  :Muhammad Shabran
+//Nim   :13020220056
+//kelas :A2
+//Kode program2
+
+public class ForEver {
+/**
+* @param args
+*/
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+/* Program */
+System.out.println("Program akan looping, akhiri dengan ^c");
+while (true)
+{ System.out.print ("Print satu baris.....\n");
+        }
+    }
+}
---
+++ Tugas3/source kode/KasusBoolean.java
@@ -0,0 +1,25 @@
+//Nama  :Muhammad Shabran
+//Nim   :13020220056
+//kelas :A2
+//Kode program6
+/* Eksrpesi kondisional dengan boolean */
+public class KasusBoolean {
+/**
+* @param args
+*/
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+/* Kamus */
+boolean bool;
+/* Algoritma */
+bool= true;
+if(bool) {
+System.out.print("true\n");
+} else
+System.out.print("false\n");
+if(!bool) {
+System.out.print("salah\n");
+} else
+System.out.print("benar\n");
+    }
+}
